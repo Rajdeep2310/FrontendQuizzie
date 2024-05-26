f639d614a68dff5f08a2b80e67c4bdcefc9713a4
Rajdeep2310 <rajdeepgkp23@gmail.com>
2024-05-26 18:40:31 +0530

    2

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    historyApiFallback: true
-  }
 })
+
+
